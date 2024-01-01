96dc633f5483896a65c6cf301c65a63f4514a8dc
Patrick Michiels <it@patrickmichiels.de>
2024-01-01 14:41:24 +0100

    added how to update info if therre are updates available

--- get_info.sh
+++ get_info.sh
@@ -21,8 +21,29 @@ who
 echo "\n\nNetwork Information:"
 ip a
 
-echo "\n\nAvailable Updates:"
-apt list --upgradable
+# Check for available updates.
+updates=$(apt list --upgradable 2>/dev/null)
+if [ -n "$updates" ]; 
+then
+    echo "\n\nAvailable Updates:"
+    
+    # Tell user how to apply them.
+    echo "\nTo update you can use this:"
+    echo "\nsudo apt-get -y update && sudo apt-get -y upgrade"
+    
+    echo "\n\nIf there are still updates remaining, try these:"
+    echo "\nsudo apt-get --with-new-pkgs upgrade <list of packages kept back>"
+    echo "\nsudo apt-get install <list of packages kept back>"
+    
+    echo "\n\nAggressive solutions are available. Read link. Try above 2 first!"
+    echo "\nhttps://askubuntu.com/questions/601/the-following-packages-have-been-kept-back-why-and-how-do-i-solve-it"
+
+    # Display the updates.
+    echo "\n$updates"
+else
+    echo "\n\nNo available updates."
+fi
+
 
 echo "\n\nLast Login Information:"
 last
