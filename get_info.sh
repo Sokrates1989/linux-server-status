45c7ac6dfe4ed6364c73496e67d367cfe680bc8b
Patrick Michiels <it@patrickmichiels.de>
2024-01-01 14:43:33 +0100

    improved update info output display

--- get_info.sh
+++ get_info.sh
@@ -28,15 +28,15 @@ then
     echo "\n\nAvailable Updates:"
     
     # Tell user how to apply them.
-    echo "\nTo update you can use this:"
-    echo "\nsudo apt-get -y update && sudo apt-get -y upgrade"
+    echo "To update you can use this:"
+    echo "sudo apt-get -y update && sudo apt-get -y upgrade"
     
-    echo "\n\nIf there are still updates remaining, try these:"
-    echo "\nsudo apt-get --with-new-pkgs upgrade <list of packages kept back>"
-    echo "\nsudo apt-get install <list of packages kept back>"
+    echo "\nIf there are still updates remaining, try these:"
+    echo "sudo apt-get --with-new-pkgs upgrade <list of packages kept back>"
+    echo "sudo apt-get install <list of packages kept back>"
     
-    echo "\n\nAggressive solutions are available. Read link. Try above 2 first!"
-    echo "\nhttps://askubuntu.com/questions/601/the-following-packages-have-been-kept-back-why-and-how-do-i-solve-it"
+    echo "\nAggressive solutions are available. Read link. Try above 2 first!"
+    echo "https://askubuntu.com/questions/601/the-following-packages-have-been-kept-back-why-and-how-do-i-solve-it"
 
     # Display the updates.
     echo "\n$updates"
